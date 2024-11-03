7cc8076b92b1e54e4cb453b82b1e3dce37ee04c3
given-dragon <yijy001@gmail.com>
2024-11-03 21:21:05 +0900

    #6 [refactor] : project not found exception 예외로 변경

--- Owing-Domain/Owing-Entity/src/main/java/com/owing/entity/domains/project/adaptor/ProjectAdaptor.java
+++ Owing-Domain/Owing-Entity/src/main/java/com/owing/entity/domains/project/adaptor/ProjectAdaptor.java
@@ -1,7 +1,7 @@
 package com.owing.entity.domains.project.adaptor;
 
 import com.owing.entity.domains.project.error.ProjectErrorCode;
-import com.owing.entity.domains.project.error.exception.ProjectException;
+import com.owing.entity.domains.project.error.exception.ProjectNotFoundException;
 import com.owing.entity.domains.project.model.Project;
 import com.owing.entity.domains.project.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +20,7 @@ public class ProjectAdaptor {
 
     public Project findById(Long projectId) {
         return projectRepository.findById(projectId)
-                .orElseThrow(() -> ProjectException.of(ProjectErrorCode.PROJECT_NOT_FOUND, "요청된 Project ID: %d".formatted(projectId)));
+                .orElseThrow(() -> ProjectNotFoundException.of(ProjectErrorCode.PROJECT_NOT_FOUND, "요청된 Project ID: %d".formatted(projectId)));
     }
 
     public List<Project> findAllByMemberId(Long memberId) {
