1917ca14673ecb9f4e3bdc53a5def6d0bd171b58
chwangmin <ckm7907@naver.com>
2024-11-05 14:14:33 +0900

    #7 [fix] : 쓰레기통 잘못된 error 코드 부분 변경

--- Owing-Domain/Owing-Entity/src/main/java/com/owing/entity/domains/trashcan/error/exception/TrashCanException.java
+++ Owing-Domain/Owing-Entity/src/main/java/com/owing/entity/domains/trashcan/error/exception/TrashCanException.java
@@ -1,19 +1,20 @@
 package com.owing.entity.domains.trashcan.error.exception;
 
 import com.owing.common.error.exception.OwingException;
+import com.owing.entity.domains.trashcan.error.TrashCanErrorCode;
 import com.owing.entity.domains.universe.error.UniverseErrorCode;
 
 public class TrashCanException extends OwingException {
 
-    private TrashCanException(UniverseErrorCode errorCode, String description) {
+    private TrashCanException(TrashCanErrorCode errorCode, String description) {
         super(errorCode, description);
     }
 
-    public static TrashCanException of(UniverseErrorCode errorCode) {
+    public static TrashCanException of(TrashCanErrorCode errorCode) {
         return new TrashCanException(errorCode, null);
     }
 
-    public static TrashCanException of(UniverseErrorCode errorCode, String description) {
+    public static TrashCanException of(TrashCanErrorCode errorCode, String description) {
         return new TrashCanException(errorCode, description);
     }
 }
