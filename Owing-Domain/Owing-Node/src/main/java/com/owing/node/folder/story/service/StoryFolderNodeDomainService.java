cafb490776e61790689ddcc9f2081a531258ebe0
given_dragon <yijy001@naver.com>
2024-11-05 12:28:10 +0900

    #12 [feat] : StoryFolderNode create, delete Domain 로직 추가

---
+++ Owing-Domain/Owing-Node/src/main/java/com/owing/node/folder/story/service/StoryFolderNodeDomainService.java
@@ -0,0 +1,29 @@
+package com.owing.node.folder.story.service;
+
+import com.owing.common.annotation.DomainService;
+import com.owing.node.folder.story.adaptor.StoryFolderNodeAdaptor;
+import com.owing.node.folder.story.model.StoryFolderNode;
+import com.owing.node.folder.story.repository.StoryFolderNodeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+@DomainService
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StoryFolderNodeDomainService {
+
+    private final StoryFolderNodeRepository storyFolderNodeRepository;
+    private final StoryFolderNodeAdaptor storyFolderNodeAdaptor;
+
+    @Transactional
+    public void createStoryFolderNode(StoryFolderNode storyFolderNode) {
+        storyFolderNodeRepository.save(storyFolderNode);
+    }
+
+    @Transactional
+    public void deleteStoryFolderNode(Long storyFolderId) {
+        StoryFolderNode storyFolderNode = storyFolderNodeAdaptor.findById(storyFolderId);
+        storyFolderNode.delete();
+        storyFolderNodeRepository.save(storyFolderNode);
+    }
+}
