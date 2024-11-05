09db2da8f5c0b7dacc00fb76edeaef87634febc9
given_dragon <yijy001@naver.com>
2024-11-05 13:59:48 +0900

    #12 [feat] : ProejctNode->StoryFolderNode Relationship 생성

--- Owing-Domain/Owing-Node/src/main/java/com/owing/node/domains/project/model/ProjectNode.java
+++ Owing-Domain/Owing-Node/src/main/java/com/owing/node/domains/project/model/ProjectNode.java
@@ -1,10 +1,15 @@
 package com.owing.node.domains.project.model;
 
 import com.owing.node.common.model.BaseTimeNeo4j;
+import com.owing.node.folder.story.model.StoryFolderNode;
 import lombok.Getter;
 import org.springframework.data.annotation.Version;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Node("Project")
 @Getter
@@ -16,8 +21,15 @@ public class ProjectNode extends BaseTimeNeo4j {
     @Version
     private Long version;
 
+    @Relationship(type = "INCLUDE", direction = Relationship.Direction.OUTGOING)
+    private Set<StoryFolderNode> storyFolderNodes = new HashSet<>();
+
     public ProjectNode(Long id) {
         this.id = id;
+    }
+
+    public void connectStoryFolder(StoryFolderNode storyFolderNode) {
+        this.storyFolderNodes.add(storyFolderNode);
     }
 
 }
--- Owing-Domain/Owing-Node/src/main/java/com/owing/node/folder/story/model/StoryFolderNode.java
+++ Owing-Domain/Owing-Node/src/main/java/com/owing/node/folder/story/model/StoryFolderNode.java
@@ -1,10 +1,12 @@
 package com.owing.node.folder.story.model;
 
 import com.owing.node.common.model.BaseTimeNeo4j;
+import com.owing.node.domains.project.model.ProjectNode;
 import lombok.Getter;
 import org.springframework.data.annotation.Version;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
 
 @Node("StoryFolder")
 @Getter
@@ -16,7 +18,14 @@ public class StoryFolderNode extends BaseTimeNeo4j {
     @Version
     private Long version;
 
+    @Relationship(type = "INCLUDE", direction = Relationship.Direction.INCOMING)
+    private ProjectNode project;
+
     public StoryFolderNode(Long id) {
         this.id = id;
+    }
+
+    public void connectProject(ProjectNode projectNode) {
+        this.project = projectNode;
     }
 }
