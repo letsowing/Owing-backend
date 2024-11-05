73fa0b8941d088fee93c1972badbd2165c873ff4
given_dragon <yijy001@naver.com>
2024-11-05 09:30:44 +0900

    #12 [feat] : ProjectNodeDomainService Transaction 추가

--- Owing-Domain/Owing-Node/src/main/java/com/owing/node/domains/project/service/ProjectNodeDomainService.java
+++ Owing-Domain/Owing-Node/src/main/java/com/owing/node/domains/project/service/ProjectNodeDomainService.java
@@ -4,13 +4,16 @@ import com.owing.common.annotation.DomainService;
 import com.owing.node.domains.project.model.ProjectNode;
 import com.owing.node.domains.project.repository.ProjectNodeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @DomainService
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProjectNodeDomainService {
 
     private final ProjectNodeRepository projectNodeRepository;
 
+    @Transactional
     public ProjectNode createProjectNode(ProjectNode projectNode) {
         return projectNodeRepository.save(projectNode);
     }
