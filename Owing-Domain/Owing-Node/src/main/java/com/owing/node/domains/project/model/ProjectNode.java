cb9673d3a3411ac01d61a862a2f79801c2ec3dff
given_dragon <yijy001@naver.com>
2024-11-05 10:41:50 +0900

    #12 [feat] : node entity 파악을 위한 Version 추가

--- Owing-Domain/Owing-Node/src/main/java/com/owing/node/domains/project/model/ProjectNode.java
+++ Owing-Domain/Owing-Node/src/main/java/com/owing/node/domains/project/model/ProjectNode.java
@@ -2,6 +2,7 @@ package com.owing.node.domains.project.model;
 
 import com.owing.node.common.model.BaseTimeNeo4j;
 import lombok.Getter;
+import org.springframework.data.annotation.Version;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 
@@ -11,6 +12,9 @@ public class ProjectNode extends BaseTimeNeo4j {
 
     @Id
     private Long id;
+
+    @Version
+    private Long version;
 
     public ProjectNode(Long id) {
         this.id = id;
