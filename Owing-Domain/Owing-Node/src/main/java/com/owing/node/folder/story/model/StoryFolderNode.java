095ea56e24e176ea936c5fd14e6690e3c34c915d
given_dragon <yijy001@naver.com>
2024-11-05 15:54:59 +0900

    #12 [feat] : StoryNode, StoryFolderNode 인터페이스 적용

--- Owing-Domain/Owing-Node/src/main/java/com/owing/node/folder/story/model/StoryFolderNode.java
+++ Owing-Domain/Owing-Node/src/main/java/com/owing/node/folder/story/model/StoryFolderNode.java
@@ -1,7 +1,9 @@
 package com.owing.node.folder.story.model;
 
 import com.owing.node.common.model.BaseTimeNeo4j;
+import com.owing.node.common.model.FolderNode;
 import com.owing.node.domains.project.model.ProjectNode;
+import com.owing.node.domains.story.model.StoryNode;
 import com.owing.node.folder.story.error.code.StoryFolderNodeErrorCode;
 import com.owing.node.folder.story.error.exception.StoryFolderNodeRelationshipException;
 import lombok.Getter;
@@ -11,9 +13,11 @@ import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.schema.Relationship;
 import org.springframework.util.ObjectUtils;
 
+import java.util.Set;
+
 @Node("StoryFolder")
 @Getter
-public class StoryFolderNode extends BaseTimeNeo4j {
+public class StoryFolderNode extends BaseTimeNeo4j implements FolderNode {
 
     @Id
     private Long id;
@@ -24,10 +28,14 @@ public class StoryFolderNode extends BaseTimeNeo4j {
     @Relationship(type = "INCLUDE", direction = Relationship.Direction.INCOMING)
     private ProjectNode project;
 
+    @Relationship(type = "INCLUDE", direction = Relationship.Direction.OUTGOING)
+    private Set<StoryNode> stories;
+
     public StoryFolderNode(Long id) {
         this.id = id;
     }
 
+    @Override
     public void connectProject(ProjectNode projectNode) {
         if (ObjectUtils.isEmpty(this.project)) {
             throw StoryFolderNodeRelationshipException.of(
