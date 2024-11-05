4e4f686e53e801dd9e37371a727e30aa0a398260
hoshogi <hoshogi@gmail.com>
2024-11-05 10:23:23 +0900

    #8 [feat] : Google ID Token 검증 Helper 추가

---
+++ Owing-Api/src/main/java/com/owing/api/auth/service/helper/GoogleOauthHelper.java
@@ -0,0 +1,27 @@
+package com.owing.api.auth.service.helper;
+
+import com.google.api.client.json.webtoken.JsonWebToken;
+import com.google.auth.oauth2.TokenVerifier;
+import com.owing.api.auth.error.exception.AuthInvalidTokenException;
+import com.owing.common.annotation.Helper;
+import lombok.RequiredArgsConstructor;
+
+import static com.owing.api.auth.error.AuthErrorCode.*;
+
+@Helper
+@RequiredArgsConstructor
+public class GoogleOauthHelper {
+
+    private final GoogleOauthProperties googleOauthProperties;
+
+    public JsonWebToken.Payload verifyGoogleIdToken(String idToken) {
+        try {
+            TokenVerifier tokenVerifier = new TokenVerifier.Builder()
+                    .setAudience(googleOauthProperties.audience())
+                    .build();
+            return tokenVerifier.verify(idToken).getPayload();
+        } catch (TokenVerifier.VerificationException e) {
+            throw AuthInvalidTokenException.of(INVALID_ID_TOKEN);
+        }
+    }
+}
