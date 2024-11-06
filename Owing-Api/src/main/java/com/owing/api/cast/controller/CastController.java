a92df11f92290f75cc89f8d3263c11d86af75665
given-dragon <yijy001@gmail.com>
2024-11-07 02:10:30 +0900

    #12 [feat] : Cast to Cast 관계 추가 api 구현

--- Owing-Api/src/main/java/com/owing/api/cast/controller/CastController.java
+++ Owing-Api/src/main/java/com/owing/api/cast/controller/CastController.java
@@ -1,8 +1,11 @@
 package com.owing.api.cast.controller;
 
 import com.owing.api.cast.model.dto.request.CreateCastRequest;
+import com.owing.api.cast.model.dto.request.CreateConnectionRequest;
 import com.owing.api.cast.model.dto.response.CastInfoResponse;
+import com.owing.api.cast.model.dto.response.CastRelationshipInfoResponse;
 import com.owing.api.cast.service.CreateCastUserCase;
+import com.owing.api.cast.service.CreateConnectionUserCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,9 +16,15 @@ import org.springframework.web.bind.annotation.*;
 public class CastController {
 
     private final CreateCastUserCase createCastUserCase;
+    private final CreateConnectionUserCase createConnectionUserCase;
 
     @PostMapping
     public ResponseEntity<CastInfoResponse> createCast(@RequestBody CreateCastRequest createCastRequest) {
         return ResponseEntity.ok(createCastUserCase.execute(createCastRequest));
+    }
+
+    @PostMapping("/relationships")
+    public ResponseEntity<CastRelationshipInfoResponse> createRelationship(@RequestBody CreateConnectionRequest createConnectionRequest) {
+        return ResponseEntity.ok(createConnectionUserCase.execute(createConnectionRequest));
     }
 }
