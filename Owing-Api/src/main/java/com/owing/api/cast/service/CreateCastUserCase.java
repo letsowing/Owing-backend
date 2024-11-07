738fab4e66c2c2ec70fa8ee6c7943aefc3511150
given-dragon <yijy001@gmail.com>
2024-11-07 13:10:50 +0900

    #12 [refactor] : Cast folder find 함수명 변경에 따른 수정 추가

--- Owing-Api/src/main/java/com/owing/api/cast/service/CreateCastUserCase.java
+++ Owing-Api/src/main/java/com/owing/api/cast/service/CreateCastUserCase.java
@@ -4,7 +4,6 @@ import com.owing.api.cast.model.dto.request.CreateCastRequest;
 import com.owing.api.cast.model.dto.response.CastInfoResponse;
 import com.owing.api.cast.model.mapper.CastNodeMapper;
 import com.owing.common.annotation.UseCase;
-import com.owing.node.domains.cast.adaptor.CastNodeAdaptor;
 import com.owing.node.domains.cast.model.CastNode;
 import com.owing.node.domains.cast.service.CastNodeDomainService;
 import com.owing.node.folder.cast.adaptor.CastFolderNodeAdaptor;
@@ -21,7 +20,7 @@ public class CreateCastUserCase {
 
     public CastInfoResponse execute(CreateCastRequest createCastRequest) {
         CastNode castNode = castNodeMapper.toEntity(createCastRequest);
-        CastFolderNode castFolderNode = castFolderNodeAdaptor.findById(createCastRequest.folderId());
+        CastFolderNode castFolderNode = castFolderNodeAdaptor.findOneById(createCastRequest.folderId());
 
         CastNode savedCastNode = castNodeDomainService.createCastNode(castNode, castFolderNode);
         return castNodeMapper.toInfoResponse(savedCastNode);
