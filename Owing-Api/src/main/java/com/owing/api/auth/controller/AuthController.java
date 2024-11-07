b1e3fff04aec76f5abda434e7eaa5a4dcd589d4a
hoshogi <hoshogi@gmail.com>
2024-11-07 20:39:37 +0900

    #22 [feat] : 로그아웃 구현
    - RefreshToken 삭제
    - RefreshToken 쿠키 삭제

--- Owing-Api/src/main/java/com/owing/api/auth/controller/AuthController.java
+++ Owing-Api/src/main/java/com/owing/api/auth/controller/AuthController.java
@@ -3,15 +3,16 @@ package com.owing.api.auth.controller;
 import com.owing.api.auth.model.dto.request.OauthLoginRequest;
 import com.owing.api.auth.model.dto.response.TokenResponse;
 import com.owing.api.auth.service.GoogleOauthLoginUseCase;
+import com.owing.api.auth.service.LogoutUseCase;
 import com.owing.api.auth.service.RefreshTokenUseCase;
 import com.owing.api.auth.service.helper.CookieHelper;
-import com.owing.api.common.constant.TokenConst;
 import com.owing.entity.domains.member.model.OauthProvider;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static com.owing.api.common.constant.TokenConst.*;
 import static org.springframework.http.HttpStatus.*;
 
 @RestController
@@ -21,6 +22,7 @@ public class AuthController {
 
     private final GoogleOauthLoginUseCase googleOauthLoginUsecase;
     private final RefreshTokenUseCase refreshTokenUseCase;
+    private final LogoutUseCase logoutUseCase;
     private final CookieHelper cookieHelper;
 
     @PostMapping("/oauth")
@@ -35,12 +37,18 @@ public class AuthController {
     }
 
     @PostMapping("/refresh")
-    ResponseEntity<TokenResponse> refreshTokens(
-            @CookieValue(value = TokenConst.REFRESH_TOKEN, required = false) String refreshToken
-    ) {
+    ResponseEntity<TokenResponse> refreshTokens(@CookieValue(value = REFRESH_TOKEN, required = false) String refreshToken) {
         TokenResponse tokenResponse = refreshTokenUseCase.execute(refreshToken);
         return ResponseEntity.ok()
                 .headers(cookieHelper.getRefreshTokenCookie(tokenResponse.refreshToken()))
                 .body(tokenResponse);
+    }
+
+    @PostMapping("/logout")
+    ResponseEntity<?> logout(@CookieValue(value = REFRESH_TOKEN, required = false) String refreshToken) {
+        logoutUseCase.execute(refreshToken);
+        return ResponseEntity.ok()
+                .headers(cookieHelper.deleteCookie())
+                .build();
     }
 }
