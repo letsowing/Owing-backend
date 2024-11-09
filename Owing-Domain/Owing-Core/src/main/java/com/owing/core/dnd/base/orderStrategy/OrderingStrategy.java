a6bc548e148b9d241958b0788b88f5040a705096
han <chos9715@naver.com>
2024-11-10 03:50:36 +0900

    #28 [feat] : 이전, 이후 엔티티 기준으로 순서 변경 상세 구현

--- Owing-Domain/Owing-Core/src/main/java/com/owing/core/dnd/base/orderStrategy/OrderingStrategy.java
+++ Owing-Domain/Owing-Core/src/main/java/com/owing/core/dnd/base/orderStrategy/OrderingStrategy.java
@@ -3,7 +3,9 @@ package com.owing.core.dnd.base.orderStrategy;
 import com.owing.core.dnd.base.model.BaseDnd;
 
 public interface OrderingStrategy<T extends BaseDnd> {
-	long getNewEntityPosition(Long parentId);
+	long getNewPosition(Long parentId);
 	void reorderEntity(T dndEntity);
-	T updatePosition(T dndEntity, long newPosition);
+	T updatePosition(T dndEntity, T beforeEntity, T afterEntity);
+	T updatePosition(T dndEntity, T beforeEntity, T afterEntity, BaseDnd newParent);
+	long getUpdatePosition(T entity, T beforeEntity, T afterEntity);
 }
--- Owing-Domain/Owing-Core/src/main/java/com/owing/core/dnd/base/orderStrategy/shift/ShiftOrderingStrategy.java
+++ Owing-Domain/Owing-Core/src/main/java/com/owing/core/dnd/base/orderStrategy/shift/ShiftOrderingStrategy.java
@@ -15,50 +15,72 @@ public abstract class ShiftOrderingStrategy<T extends BaseDnd> implements Orderi
 	protected final BaseDndRepository<T> dndRepository;
 
 	@Override
-	public long getNewEntityPosition(Long parentId){
+	public long getNewPosition(Long parentId) {
 		return getLastPosition(parentId) + 1;
 	}
 
-	protected abstract T handleEntityUpdate(T entity, long newPosition);
+	public long getUpdatePosition(T entity, T beforeEntity, T afterEntity) {
+		if(beforeEntity != null){
+			if (beforeEntity.getParentId().equals(entity.getParentId()) && beforeEntity.getPosition() > entity.getPosition()) { // 현재 위치보다 아래로
+				return beforeEntity.getPosition();
+			}
+			return beforeEntity.getPosition() + 1; 		// 현재 위치보다 위로
+		} else {
+			if (afterEntity.getParentId().equals(entity.getParentId()) && afterEntity.getPosition() > entity.getPosition()){
+				return afterEntity.getPosition() - 1;
+			}
+			return afterEntity.getPosition();
+		}
+	}
+
+	protected abstract T handleEntityUpdate(T entity, T beforeEntity, T afterEntity);
 
 	@Override
-	public void reorderEntity(T entity){
+	public void reorderEntity(T entity) {
 		shiftFolderUp(entity.getPosition(), entity.getParentId());
 	}
 
 	@Override
-	public T updatePosition(T entity, long newPosition) {
-		long maxPosition = getLastPosition(entity.getParentId());
-		if(!validateEntityPosition(newPosition, maxPosition)){
+	public T updatePosition(T entity, T beforeEntity, T afterEntity) {
+		if (!validateEntityPosition(entity, beforeEntity, afterEntity)) {
 			throw DndInvalidPositionException.of(DndErrorCode.INVALID_POSITION);
 		}
-
-		return handleEntityUpdate(entity, newPosition);
+		return handleEntityUpdate(entity, beforeEntity, afterEntity);
 	}
 
-	protected boolean validateEntityPosition(long newPosition, long maxPosition) {
-		return newPosition <= maxPosition;
+	@Override
+	public T updatePosition(T dndEntity, T beforeEntity, T afterEntity, BaseDnd newParent) {
+		return updatePosition(dndEntity, beforeEntity, afterEntity);
 	}
 
-	protected void moveFolderUp(long newPosition, long oldPosition, Long projectId){
+	protected abstract boolean validateEntityPosition(T entity, T beforeEntity, T afterEntity);
+
+	protected void moveFolderUp(long newPosition, long oldPosition, Long projectId) {
 		dndAdapter.incrementPositionBetween(newPosition, oldPosition - 1, projectId);
 	}
 
-	protected void moveFolderDown(long newPosition, long oldPosition, Long projectId){
+	protected void moveFolderDown(long newPosition, long oldPosition, Long projectId) {
 		dndAdapter.decrementPositionBetween(oldPosition + 1, newPosition, projectId);
 	}
 
-	protected void shiftFolderUp(long targetPosition, Long projectId){
+	protected void shiftFolderUp(long targetPosition, Long projectId) {
 		dndAdapter.decrementPositionAfter(targetPosition, projectId);
 	}
 
-	protected void shiftFolderDown(long targetPosition, Long projectId){
+	protected void shiftFolderDown(long targetPosition, Long projectId) {
 		dndAdapter.decrementPositionAfter(targetPosition, projectId);
 	}
 
-	protected long getLastPosition(Long projectId){
+	protected long getLastPosition(Long projectId) {
 		return dndRepository.getMaxPositionByParentId(projectId);
 	}
 
+	protected boolean hasSameParentFolder(T e1, T e2) {
+		return Objects.equals(e1.getParentId(), e2.getParentId());
+	}
+
+	protected boolean isSequentialPosition(T beforeEntity, T afterEntity) {
+		return afterEntity.getPosition() - beforeEntity.getPosition() == 1;
+	}
 
 }
