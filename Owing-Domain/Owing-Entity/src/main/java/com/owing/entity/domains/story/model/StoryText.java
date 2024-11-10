fc1d18719005dc3d65fc51f63ab15a9d2125f216
han <chos9715@naver.com>
2024-11-11 00:16:29 +0900

    #35 [feat] : storyText 엔티티 추가

---
+++ Owing-Domain/Owing-Entity/src/main/java/com/owing/entity/domains/story/model/StoryText.java
@@ -0,0 +1,40 @@
+package com.owing.entity.domains.story.model;
+
+import org.hibernate.annotations.SoftDelete;
+
+import com.owing.entity.common.model.BaseTimeEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SoftDelete
+public class StoryText extends BaseTimeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne(mappedBy = "storyText")
+	private Story story;
+
+	private String content;
+
+	@Builder
+	public StoryText(Story story, String content) {
+		this.story = story;
+		this.content = content;
+	}
+
+	public void update(StoryText newStoryText) {
+        this.content = newStoryText.content;
+    }
+}
