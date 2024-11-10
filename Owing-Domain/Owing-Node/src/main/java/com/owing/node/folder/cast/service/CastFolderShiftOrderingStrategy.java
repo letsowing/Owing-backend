5861dd2f00d87fb94139dc5fe60ef57898eebb92
given-dragon <yijy001@gmail.com>
2024-11-11 02:23:49 +0900

    #26 [refactor] : CastFolderShiftOrderingStrategy 생성

---
+++ Owing-Domain/Owing-Node/src/main/java/com/owing/node/folder/cast/service/CastFolderShiftOrderingStrategy.java
@@ -0,0 +1,15 @@
+package com.owing.node.folder.cast.service;
+
+import com.owing.core.dnd.base.orderStrategy.shift.FolderShiftOrderingStrategy;
+import com.owing.node.folder.cast.adaptor.CastFolderNodeAdaptor;
+import com.owing.node.folder.cast.model.CastFolderNode;
+import com.owing.node.folder.cast.repository.CastFolderNodeRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CastFolderShiftOrderingStrategy extends FolderShiftOrderingStrategy<CastFolderNode> {
+	public CastFolderShiftOrderingStrategy(CastFolderNodeAdaptor dndAdapter,
+										   CastFolderNodeRepository dndRepository) {
+		super(dndAdapter, dndRepository);
+	}
+}
