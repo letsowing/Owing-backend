ac5f23bc7c9c7fab9815a6607a28309ac2280618
given-dragon <yijy001@gmail.com>
2024-11-11 04:07:46 +0900

    #26 [refactor] : CastFolder read dnd core 코드 적용

--- Owing-Api/src/main/java/com/owing/api/cast/controller/CastFolderController.java
+++ Owing-Api/src/main/java/com/owing/api/cast/controller/CastFolderController.java
@@ -1,18 +1,16 @@
 package com.owing.api.cast.controller;
 
 import com.owing.api.cast.model.dto.request.UpdateCastFolderInfo;
-import com.owing.api.cast.model.dto.response.CastFolderResponse;
 import com.owing.api.cast.service.CreateCastFolderUseCase;
 import com.owing.api.cast.service.DeleteCastFolderUseCase;
 import com.owing.api.cast.service.ReadCastFolderUseCase;
 import com.owing.api.cast.service.UpdateCastFolderUseCase;
 import com.owing.api.dnd.folder.model.dto.request.AddFolderRequest;
+import com.owing.api.dnd.folder.model.dto.response.FolderInfoListResponse;
 import com.owing.api.dnd.folder.model.dto.response.FolderInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/v1/cast/folders")
@@ -30,13 +28,13 @@ public class CastFolderController {
     }
 
     @GetMapping
-    public ResponseEntity<List<CastFolderResponse>> getCastFolderList(@RequestParam Long projectId) {
-        return ResponseEntity.ok(readCastFolderUseCase.executeGetList(projectId));
+    public ResponseEntity<FolderInfoListResponse> getCastFolderList(@RequestParam Long projectId) {
+        return ResponseEntity.ok(readCastFolderUseCase.executeList(projectId));
     }
 
     @GetMapping("/{folderId}")
-    public ResponseEntity<CastFolderResponse> getCastFolder(@PathVariable Long folderId) {
-        return ResponseEntity.ok(readCastFolderUseCase.executeGetOne(folderId));
+    public ResponseEntity<FolderInfoResponse> getCastFolder(@PathVariable Long folderId) {
+        return ResponseEntity.ok(readCastFolderUseCase.executeRetrieve(folderId));
     }
 
     @PutMapping("/{folderId}")
