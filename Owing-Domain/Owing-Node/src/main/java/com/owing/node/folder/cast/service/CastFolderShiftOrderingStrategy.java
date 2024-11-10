f01a0b6d6ff1e063b88903cb2a3956d90eacc8c4
given-dragon <yijy001@gmail.com>
2024-11-11 05:48:17 +0900

    #26 [refactor] : CastFolderShiftOrderingStrategy.handleEntityUpdate 저장 방식 변경

--- Owing-Domain/Owing-Node/src/main/java/com/owing/node/folder/cast/service/CastFolderShiftOrderingStrategy.java
+++ Owing-Domain/Owing-Node/src/main/java/com/owing/node/folder/cast/service/CastFolderShiftOrderingStrategy.java
@@ -3,15 +3,35 @@ package com.owing.node.folder.cast.service;
 import com.owing.core.dnd.base.orderStrategy.shift.FolderShiftOrderingStrategy;
 import com.owing.node.folder.cast.adaptor.CastFolderNodeAdaptor;
 import com.owing.node.folder.cast.model.CastFolderNode;
+import com.owing.node.folder.cast.model.projection.CastFolderPositionProjection;
 import com.owing.node.folder.cast.repository.CastFolderNodeRepository;
+import org.springframework.data.neo4j.core.Neo4jTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CastFolderShiftOrderingStrategy extends FolderShiftOrderingStrategy<CastFolderNode> {
+	private final Neo4jTemplate neo4jTemplate;
+
 	public CastFolderShiftOrderingStrategy(
 			CastFolderNodeAdaptor dndAdapter,
-			CastFolderNodeRepository dndRepository
-	) {
+			CastFolderNodeRepository dndRepository,
+			Neo4jTemplate neo4jTemplate) {
 		super(dndAdapter, dndRepository);
+		this.neo4jTemplate = neo4jTemplate;
+	}
+
+	@Override
+	protected CastFolderNode handleEntityUpdate(CastFolderNode entity, CastFolderNode beforeEntity, CastFolderNode afterEntity) {
+		long newPosition = getUpdatePosition(entity, beforeEntity, afterEntity);
+
+		if (entity.getPosition() < newPosition) {
+			moveFolderDown(newPosition, entity.getPosition(), entity.getParentId());
+		} else {
+			moveFolderUp(newPosition, entity.getPosition(), entity.getParentId());
+		}
+		entity.updatePosition(newPosition);
+		CastFolderPositionProjection castFolderPositionProjection = CastFolderPositionProjection.from(entity);
+		neo4jTemplate.save(CastFolderNode.class).one(castFolderPositionProjection);
+		return entity;
 	}
 }
