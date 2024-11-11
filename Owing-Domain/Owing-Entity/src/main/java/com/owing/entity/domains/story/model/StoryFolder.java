fd7c1c20ea52c56a85e3678d41b26769676c5488
given-dragon <yijy001@gmail.com>
2024-11-11 15:41:35 +0900

    #42 [refactor] : 각 Entity에 files 연관관계 생성, FolderInfoResponse에 files 필드 추가

--- Owing-Domain/Owing-Entity/src/main/java/com/owing/entity/domains/story/model/StoryFolder.java
+++ Owing-Domain/Owing-Entity/src/main/java/com/owing/entity/domains/story/model/StoryFolder.java
@@ -1,17 +1,18 @@
 package com.owing.entity.domains.story.model;
 
+import com.owing.core.dnd.file.model.BaseFileEntity;
+import jakarta.persistence.*;
 import org.hibernate.annotations.SoftDelete;
 
 import com.owing.core.dnd.folder.model.BaseFolderEntity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,6 +22,9 @@ public class StoryFolder extends BaseFolderEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@OneToMany(mappedBy = "folder", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+	protected List<Story> files = new ArrayList<>();
 
 	@Builder
 	StoryFolder(Long projectId, String name, String description, Long position) {
