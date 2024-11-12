20f332e6a6bc6d498c0c44dc5b8dbbf0703d4355
given-dragon <yijy001@gmail.com>
2024-11-12 22:05:22 +0900

    #47 [feat] : cast graph 조회 api 구현

--- Owing-Api/src/main/java/com/owing/api/cast/controller/CastController.java
+++ Owing-Api/src/main/java/com/owing/api/cast/controller/CastController.java
@@ -1,13 +1,9 @@
 package com.owing.api.cast.controller;
 
+import com.owing.api.cast.model.dto.response.CastGraphResponse;
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.owing.api.cast.model.dto.request.CreateCastRequest;
 import com.owing.api.cast.model.dto.request.CreateConnectionRequest;
@@ -44,6 +40,12 @@ public class CastController extends BaseFileController {
     private final ReadCastUseCase readCastUseCase;
     private final UpdateCastUseCase updateCastUseCase;
     private final DeleteCastUseCase deleteCastUseCase;
+
+    @GetMapping("/graph")
+    @Operation(summary = "✨ 관계도: 캐릭터 ", description = "인물관계도 조회")
+    public ResponseEntity<CastGraphResponse> getGraph(@RequestParam Long projectId) {
+        return ResponseEntity.ok(readCastUseCase.executeGraph(projectId));
+    }
 
     @PutMapping("/{castId}")
     @Operation(summary = "✨ 일반: 캐릭터 정보 수정", description = "캐릭터 정보 수정")
--- Owing-Api/src/main/java/com/owing/api/cast/service/ReadCastUseCase.java
+++ Owing-Api/src/main/java/com/owing/api/cast/service/ReadCastUseCase.java
@@ -1,13 +1,18 @@
 package com.owing.api.cast.service;
 
+import com.owing.api.cast.model.dto.response.CastGraphResponse;
 import com.owing.api.cast.model.dto.response.CastInfoResponse;
 import com.owing.api.cast.model.mapper.CastNodeMapper;
 import com.owing.api.dnd.base.service.ReadDndUseCase;
 import com.owing.common.annotation.UseCase;
 import com.owing.node.domains.cast.model.CastNode;
+import com.owing.node.domains.cast.model.projection.CastGraphNodeProjection;
+import com.owing.node.domains.cast.model.projection.CastGraphRelationshipProjection;
 import com.owing.node.domains.cast.service.CastNodeDomainService;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @UseCase
 @RequiredArgsConstructor
@@ -19,5 +24,11 @@ public class ReadCastUseCase implements ReadDndUseCase {
     public CastInfoResponse executeRetrieve(Long castId){
         CastNode castnode = castNodeDomainService.getEntity(castId);
         return castNodeMapper.toInfoResponse(castnode);
+    }
+
+    public CastGraphResponse executeGraph(Long projectId) {
+        List<CastGraphNodeProjection> graphNode = castNodeDomainService.getGraphNode(projectId);
+        List<CastGraphRelationshipProjection> graphRelationship = castNodeDomainService.getGraphRelationship(projectId);
+        return castNodeMapper.toGraphResponse(graphNode, graphRelationship);
     }
 }
