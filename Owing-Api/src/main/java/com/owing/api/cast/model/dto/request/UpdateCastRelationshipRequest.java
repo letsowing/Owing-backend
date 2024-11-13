d9bb26422b5e1a565f390b85490da6f099a868b0
given-dragon <yijy001@gmail.com>
2024-11-13 17:01:54 +0900

    [hotfix] : update cast relationship json alias 추가

--- Owing-Api/src/main/java/com/owing/api/cast/model/dto/request/UpdateCastRelationshipRequest.java
+++ Owing-Api/src/main/java/com/owing/api/cast/model/dto/request/UpdateCastRelationshipRequest.java
@@ -1,11 +1,14 @@
 package com.owing.api.cast.model.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.owing.node.domains.cast.model.ConnectionHandle;
 import com.owing.node.domains.cast.model.ConnectionType;
 
 public record UpdateCastRelationshipRequest(
         ConnectionType type,
+        @JsonAlias("source")
         Long sourceId,
+        @JsonAlias("target")
         Long targetId,
         ConnectionHandle sourceHandle,
         ConnectionHandle targetHandle
