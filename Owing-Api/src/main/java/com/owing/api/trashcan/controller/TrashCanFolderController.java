32a91d5516aee3ef2e98eafb53755a54003b8cb8
chwangmin <ckm7907cb@gmail.com>
2024-11-15 21:02:03 +0900

    #58 [docs] : swagger 설명 추가

--- Owing-Api/src/main/java/com/owing/api/trashcan/controller/TrashCanFolderController.java
+++ Owing-Api/src/main/java/com/owing/api/trashcan/controller/TrashCanFolderController.java
@@ -1,5 +1,7 @@
 package com.owing.api.trashcan.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/v1/trashcans/folders")
 @RequiredArgsConstructor
+@Tag(name="쓰레기_폴더 /trashcans/folders", description="쓰레기_폴더 API")
 public class TrashCanFolderController {
 	private final CreateTrashCanFolderUserCase createTrashCanFolderUserCase;
 	private final ReadTrashCanFolderUserCase readTrashCanFolderUserCase;
@@ -28,28 +31,33 @@ public class TrashCanFolderController {
 	private final DeleteTrashCanFolderUserCase deleteTrashCanFolderUserCase;
 
 	@GetMapping
-	public ResponseEntity<TrashCanFolderResponse> getTrashCan(@RequestParam("projectId") Long projectId) {
+	@Operation(summary = "✨일반: 쓰레기_폴더 모두보기", description = "프로젝트에 해당하는 쓰레기 리스트를 모두 확인합니다.")
+	public ResponseEntity<TrashCanFolderResponse> getTrashCanFolder(@RequestParam("projectId") Long projectId) {
 		return ResponseEntity.ok(readTrashCanFolderUserCase.executeProject(projectId));
 	}
 
 	@DeleteMapping
+	@Operation(summary = "✨일반: 쓰레기_폴더 모두 삭제하기", description = "프로젝트에 해당하는 모든 쓰레기를 영구 삭제합니다.")
 	public ResponseEntity<?> deleteAllTrashCan(@RequestParam("projectId") Long projectId) {
 		deleteTrashCanFolderUserCase.executeAll(projectId);
 		return ResponseEntity.noContent().build();
 	}
 
 	@GetMapping("/{trashId}")
+	@Operation(summary = "✨일반: 쓰레기_폴더 상세보기", description = "쓰레기_폴더를 상세하게 봅니다.")
 	public ResponseEntity<TrashCanFolder> getFolderTrashCan(@PathVariable Long trashId) {
 		return ResponseEntity.ok(readTrashCanFolderUserCase.execute(trashId));
 	}
 
 	@DeleteMapping("/{trashId}")
+	@Operation(summary = "✨일반: 쓰레기_폴더 영구 삭제", description = "쓰레기_폴더를 영구적으로 삭제합니다.")
 	public ResponseEntity<?> deleteTrashCanFolder(@PathVariable Long trashId) {
 		deleteTrashCanFolderUserCase.executeFolder(trashId);
 		return ResponseEntity.noContent().build();
 	}
 
 	@PostMapping("/{trashId}")
+	@Operation(summary = "✨일반: 쓰레기_폴더 복원하기", description = "쓰레기_폴더를 복원합니다.")
 	public ResponseEntity<?> restoreTrashCanFolder(@PathVariable Long trashId) {
 		updateTrashCanFolderUserCase.executeRestoreFolder(trashId);
 		return ResponseEntity.noContent().build();
