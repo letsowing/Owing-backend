a0e1686e62889f6efc8ba4563bdb1a9961d0d37b
chwangmin <ckm7907cb@gmail.com>
2024-11-15 19:44:38 +0900

    #58 [fix] : cast 부분 삭제될때 files 안가져오는 부분 해결

--- Owing-Api/src/main/java/com/owing/api/cast/service/DeleteCastFolderUseCase.java
+++ Owing-Api/src/main/java/com/owing/api/cast/service/DeleteCastFolderUseCase.java
@@ -3,11 +3,12 @@ package com.owing.api.cast.service;
 import com.owing.api.common.util.MemberUtils;
 import com.owing.api.dnd.folder.service.DeleteFolderUseCase;
 import com.owing.api.trashcan.model.mapper.TrashCanFolderMapper;
-import com.owing.api.trashcan.model.mapper.TrashCanMapper;
 import com.owing.common.annotation.UseCase;
 import com.owing.core.dnd.base.service.BaseDndDomainService;
 import com.owing.entity.domains.project.adapter.ProjectAdapter;
+import com.owing.entity.domains.project.model.Project;
 import com.owing.entity.folders.trashcan.service.TrashCanFolderDomainService;
+import com.owing.node.folder.cast.adapter.CastFolderNodeAdapter;
 import com.owing.node.folder.cast.model.CastFolderNode;
 import com.owing.node.folder.cast.service.CastFolderNodeDomainService;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +22,16 @@ public class DeleteCastFolderUseCase extends DeleteFolderUseCase<CastFolderNode>
     private final TrashCanFolderMapper trashCanFolderMapper;
     private final ProjectAdapter projectAdapter;
     private final MemberUtils memberUtils;
+    private final CastFolderNodeAdapter castFolderNodeAdapter;
+
+    @Override
+    public void execute(Long folderId) {
+        // Long memberId = memberUtils.getCurrentMemberId();
+        CastFolderNode castFolderNode = castFolderNodeAdapter.findOneWithRelationshipById(folderId);
+        Project project = projectAdapter().findById(castFolderNode.getProjectId());
+        trashCanFolderDomainService().createTrashCanFolder(trashCanFolderMapper().toFolderEntity(castFolderNode, project));
+        baseDndDomainService().deleteEntity(castFolderNode);
+    }
 
     @Override
     protected MemberUtils memberUtils() {
