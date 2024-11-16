490123f60f40c4b7c99559aa0e8b8aa40b1bd1e0
han <chos9715@naver.com>
2024-11-16 15:51:46 +0900

    #76 [feat] : 원고 내용 작성시 글자수 업데이트

--- Owing-Api/src/main/java/com/owing/api/story/service/story/CreateStoryUseCase.java
+++ Owing-Api/src/main/java/com/owing/api/story/service/story/CreateStoryUseCase.java
@@ -4,15 +4,17 @@ import com.owing.api.common.util.MemberUtils;
 import com.owing.api.dnd.file.model.mapper.BaseFileMapper;
 import com.owing.api.dnd.file.service.CreateFileUseCase;
 import com.owing.api.story.model.dto.request.AddStoryContentRequest;
-import com.owing.api.story.model.mapper.StoryMapper;
 import com.owing.api.story.model.mapper.StoryContentMapper;
+import com.owing.api.story.model.mapper.StoryMapper;
 import com.owing.common.annotation.UseCase;
 import com.owing.core.dnd.base.adapter.BaseDndAdapter;
 import com.owing.core.dnd.base.service.BaseDndDomainService;
+import com.owing.entity.domains.member.model.Member;
+import com.owing.entity.domains.statistics.service.DashboardService;
 import com.owing.entity.domains.story.adapter.StoryFolderAdapter;
 import com.owing.entity.domains.story.model.Story;
-import com.owing.entity.domains.story.model.StoryFolder;
 import com.owing.entity.domains.story.model.StoryContent;
+import com.owing.entity.domains.story.model.StoryFolder;
 import com.owing.entity.domains.story.service.StoryDomainService;
 
 import lombok.RequiredArgsConstructor;
@@ -25,12 +27,19 @@ public class CreateStoryUseCase extends CreateFileUseCase<Story, StoryFolder> {
     private final StoryMapper storyMapper;
     private final StoryContentMapper storyContentMapper;
     private final StoryFolderAdapter folderAdapter;
+    private final DashboardService dashboardService;
 
     public void executeText(Long storyId, AddStoryContentRequest request) {
+        Member member = memberUtils.getCurrentMember();
         Story story = storyDomainService.getEntity(storyId);
         StoryContent storyContent = storyContentMapper.toEntity(request.content(), story);
-        storyDomainService.updateContent(story, storyContent);
-        story.createOrUpdateStoryText(storyContent);
+        int beforeLength = story.getTextCount();
+
+        storyDomainService.updateContent(story, storyContent); // 내용 작성
+
+        int afterLength = story.getTextCount();
+
+        dashboardService.updateTodayCount(member, afterLength - beforeLength);
     }
 
     @Override
