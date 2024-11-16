dbda93fdaa696bdf59086377475fe5e31b41dd8f
han <chos9715@naver.com>
2024-11-16 15:49:26 +0900

    #76 [feat] : 작업량 entity 추가

---
+++ Owing-Domain/Owing-Entity/src/main/java/com/owing/entity/domains/statistics/model/MonthlyStats.java
@@ -0,0 +1,43 @@
+package com.owing.entity.domains.statistics.model;
+
+import java.time.LocalDate;
+
+import com.owing.entity.common.model.BaseTimeEntity;
+import com.owing.entity.domains.member.model.Member;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MonthlyStats extends BaseTimeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
+	private LocalDate month;
+	private Integer monthlyCount;
+
+	public void updateMonthlyCount(int monthlyCount) {
+        if(monthlyCount < 0){
+            throw new IllegalArgumentException("T.T"); //fixme
+        }
+		this.monthlyCount = monthlyCount;
+	}
+}
