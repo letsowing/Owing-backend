afc45c32a71a4be4f242c8b8668cc0d8f077a187
han <chos9715@naver.com>
2024-11-16 16:14:23 +0900

    #79 [refactor] : 변수명 수정

--- Owing-Domain/Owing-Entity/src/main/java/com/owing/entity/domains/story/service/StoryFolderDomainService.java
+++ Owing-Domain/Owing-Entity/src/main/java/com/owing/entity/domains/story/service/StoryFolderDomainService.java
@@ -10,22 +10,19 @@ import com.owing.core.dnd.folder.service.BaseFolderDomainService;
 import com.owing.entity.domains.story.adapter.StoryFolderAdapter;
 import com.owing.entity.domains.story.model.StoryFolder;
 import com.owing.entity.domains.story.repository.StoryFolderRepository;
-import com.owing.entity.domains.story.repository.StoryRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @DomainService
 @RequiredArgsConstructor
 public class StoryFolderDomainService extends BaseFolderDomainService<StoryFolder> {
-	private final StoryFolderRepository dndRepository;
 	private final StoryFolderAdapter dndAdapter;
 	private final StoryFolderShiftOrderingStrategy orderingStrategy;
 	private final StoryFolderRepository storyFolderRepository;
-	private final StoryRepository storyRepository;
 
 	@Override
 	protected BaseDndRepository<StoryFolder> dndRepository() {
-		return dndRepository;
+		return storyFolderRepository;
 	}
 
 	@Override
