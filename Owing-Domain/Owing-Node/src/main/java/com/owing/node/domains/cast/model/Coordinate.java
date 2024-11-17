29679ff866c059060e60b524011d22ad45d8f2f2
given-dragon <yijy001@gmail.com>
2024-11-17 22:02:10 +0900

    #82 [feat] : Cast 노드의 좌표 정보가 없을시 Random하게 생성하도록 설정

--- Owing-Domain/Owing-Node/src/main/java/com/owing/node/domains/cast/model/Coordinate.java
+++ Owing-Domain/Owing-Node/src/main/java/com/owing/node/domains/cast/model/Coordinate.java
@@ -1,7 +1,16 @@
 package com.owing.node.domains.cast.model;
 
+import com.owing.node.common.constant.CastConstant;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public record Coordinate(
         Integer x,
         Integer y
 ) {
+
+    public static int randomCoordinate() {
+        return ThreadLocalRandom.current().nextInt(0, CastConstant.MAX_COORDINATE +1);
+    }
+
 }
