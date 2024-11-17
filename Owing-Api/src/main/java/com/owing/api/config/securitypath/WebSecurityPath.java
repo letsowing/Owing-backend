f133b81ae56d803c3dda508eb79b72cc218d40d7
한소연 <chos9715@naver.com>
2024-11-17 12:42:51 +0900

    [hotfix] : security path 추가

--- Owing-Api/src/main/java/com/owing/api/config/securitypath/WebSecurityPath.java
+++ Owing-Api/src/main/java/com/owing/api/config/securitypath/WebSecurityPath.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum WebSecurityPath {
-	REQUIRE_AUTH_PATH("/v1/members/**", "/v1/projects/**", "/v1/stories/**", "/v1/cast/**", "/v1/universes/**", "/v1/trashcans/**");
+	REQUIRE_AUTH_PATH("/v1/members/**", "/v1/projects/**", "/v1/stories/**", "/v1/cast/**", "/v1/universes/**", "/v1/trashcans/**", "/v1/dashboard");
 
 	private final String[] paths;
 
