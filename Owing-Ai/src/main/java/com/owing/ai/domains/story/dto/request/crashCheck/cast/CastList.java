c0b14d7a1b87f990eceeb080ef0574219cb3f94f
han <chos9715@naver.com>
2024-11-18 15:45:28 +0900

    #44 [refactor] : toString 추가

--- Owing-Ai/src/main/java/com/owing/ai/domains/story/dto/request/crashCheck/cast/CastList.java
+++ Owing-Ai/src/main/java/com/owing/ai/domains/story/dto/request/crashCheck/cast/CastList.java
@@ -13,4 +13,14 @@ public record CastList(
 	String description,
 	LocalDateTime updatedAt
 ) implements StoryInfoDto {
+
+	@Override
+	public String toString() {
+		return String.format(
+			"""
+				[등장인물]
+				이름 : %s, 역할 : %s, 성별 : %s, 나이 : %d, 설명 : %s
+			""",
+			name, role, gender, age, description);
+	}
 }
