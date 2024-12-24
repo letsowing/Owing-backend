ac29422c764dd64b874dbdae2ab0d91ac8a6e685
han <chos9715@naver.com>
2024-12-25 06:09:20 +0900

    [refactor] : BaseFile -> DndFile 변경

--- Owing-Domain/Owing-Core/src/main/java/com/owing/core/dnd/file/repository/BaseFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.owing.core.dnd.file.repository;
-
-import com.owing.core.dnd.base.repository.BaseDndRepository;
-import com.owing.core.dnd.file.model.BaseFile;
-import com.owing.core.dnd.folder.model.BaseFolder;
-
-public interface BaseFileRepository<T extends BaseFile<F>, F extends BaseFolder> extends BaseDndRepository<T> {
-    void incrementPositionAfter(Long targetPosition, Long projectId);
-}
---
+++ Owing-Domain/Owing-Core/src/main/java/com/owing/core/dnd/file/service/DndFileDomainService.java
@@ -0,0 +1,7 @@
+package com.owing.core.dnd.file.service;
+
+import com.owing.core.dnd.base.service.DndDomainService;
+import com.owing.core.dnd.file.model.DndFile;
+
+public abstract class DndFileDomainService<T extends DndFile> extends DndDomainService<T> {
+}
