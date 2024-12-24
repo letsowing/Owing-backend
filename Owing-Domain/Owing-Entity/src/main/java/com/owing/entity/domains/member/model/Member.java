a4dbece8aab3c4a8b66e25c3ea9c5e90d4966bd1
han <chos9715@naver.com>
2024-12-25 06:14:41 +0900

    [refactor] : [domain-entity] BaseTimeEntity -> BaseEntity

--- Owing-Domain/Owing-Entity/src/main/java/com/owing/entity/domains/member/model/Member.java
+++ Owing-Domain/Owing-Entity/src/main/java/com/owing/entity/domains/member/model/Member.java
@@ -1,21 +1,32 @@
 package com.owing.entity.domains.member.model;
 
-import com.owing.entity.common.model.BaseTimeEntity;
-import com.owing.entity.domains.project.model.Project;
-import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.annotations.SoftDelete;
+import static com.owing.core.constant.OwingPersistenceConst.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.owing.core.constant.OwingPersistenceConst.*;
+import com.owing.core.BaseEntity;
+import com.owing.entity.domains.project.model.Project;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@SoftDelete
-public class Member extends BaseTimeEntity {
+public class Member extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
