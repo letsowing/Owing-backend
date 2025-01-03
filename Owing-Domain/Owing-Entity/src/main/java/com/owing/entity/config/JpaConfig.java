3ed3356c16d7f2a3a00cb460efc56edfbf043613
given-dragon <yijy001@naver.com>
2025-01-03 16:45:07 +0900

    #7 [refactor]: db별 txManager bean name 지정

--- Owing-Domain/Owing-Entity/src/main/java/com/owing/entity/config/JpaConfig.java
+++ Owing-Domain/Owing-Entity/src/main/java/com/owing/entity/config/JpaConfig.java
@@ -1,15 +1,28 @@
 package com.owing.entity.config;
 
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaAuditing
 @EnableTransactionManagement
 @EntityScan("com.owing.entity")
-@EnableJpaRepositories("com.owing.entity")
+@EnableJpaRepositories(
+        value = "com.owing.entity",
+        transactionManagerRef = "jpaTransactionManager"
+)
 public class JpaConfig {
+
+    // DB별 txManager를 구분하기 위해 별도의 Bean name으로 지정
+    @Bean
+    protected PlatformTransactionManager jpaTransactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
 }
