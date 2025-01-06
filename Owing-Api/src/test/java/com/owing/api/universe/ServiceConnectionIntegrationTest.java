d19a4b7acaf0c459e4b65e6aa620b09018d8371c
chris <chris2769@naver.com>
2025-01-06 17:07:06 +0900

    #2 [test] : testContainer 생성 코드 작성

--- Owing-Api/src/test/java/com/owing/api/universe/ServiceConnectionIntegrationTest.java
+++ Owing-Api/src/test/java/com/owing/api/universe/ServiceConnectionIntegrationTest.java
@@ -1,4 +1,31 @@
 package com.owing.api.universe;
 
+import com.owing.OwingApiApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(
+        classes = OwingApiApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@Testcontainers
 public class ServiceConnectionIntegrationTest {
+
+    @Container
+    @ServiceConnection
+    private static final PostgreSQLContainer<?> postgresContainer =
+            new PostgreSQLContainer<>("postgres:15.2")
+                    .withDatabaseName("testdb")
+                    .withUsername("testuser")
+                    .withPassword("testpass");
+
+    @Container
+    @ServiceConnection
+    private static final GenericContainer<?> redisContainer =
+            new GenericContainer<>("redis:7.0")
+                    .withExposedPorts(6379);
 }
