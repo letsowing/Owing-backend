31f264ea55d7ef26817e3fcbfe6d8abb850f4dbc
chris <chris2769@naver.com>
2025-01-22 01:15:28 +0900

    #2 [refactor] : Request Dto에 대한 별도의 검증 추가 ( NotBlank 및 NotNull) | 코드 리뷰 피드백 반영

--- Owing-Api/src/main/java/com/owing/api/universe/model/dto/request/GenerateUniverseImageRequest.java
+++ Owing-Api/src/main/java/com/owing/api/universe/model/dto/request/GenerateUniverseImageRequest.java
@@ -1,6 +1,12 @@
 package com.owing.api.universe.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record GenerateUniverseImageRequest(
+
+	@NotBlank(message = "세계관 이름은 필수적으로 들어가야 합니다.")
 	String name,
+
+	@NotBlank(message = "세계관 설명은 필수적으로 들어가야 합니다.")
 	String description
 ) { }
