7e08df940380032f49449fd40a11c14b3afd3af2
chris <chris2769@naver.com>
2025-01-22 01:16:10 +0900

    #2 [refactor] : UpdateUniverseRequest 에 대한 별도의 검증 추가 ( NotBlank 및 NotNull) | 코드 리뷰 피드백 반영

--- Owing-Api/src/main/java/com/owing/api/universe/model/dto/request/UpdateUniverseRequest.java
+++ Owing-Api/src/main/java/com/owing/api/universe/model/dto/request/UpdateUniverseRequest.java
@@ -1,7 +1,14 @@
 package com.owing.api.universe.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record UpdateUniverseRequest(
+
+	@NotBlank(message = "세계관 이름은 필수적으로 들어가야합니다.")
 	String name,
+
+	@NotBlank(message = "세계관 설명은 필수적으로 들어가야합니다.")
 	String description,
+
 	String imageUrl
 ) { }
