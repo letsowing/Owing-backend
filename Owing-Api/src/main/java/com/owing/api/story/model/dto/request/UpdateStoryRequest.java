d4e31b82f2686360efb67d71d611067320265a4f
han <chos9715@naver.com>
2025-03-23 04:23:10 +0900

    #23 [test] : Story 정보 수정 Request DTO Validation 추가

--- Owing-Api/src/main/java/com/owing/api/story/model/dto/request/UpdateStoryRequest.java
+++ Owing-Api/src/main/java/com/owing/api/story/model/dto/request/UpdateStoryRequest.java
@@ -1,6 +1,11 @@
 package com.owing.api.story.model.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record UpdateStoryRequest(
+	@NotNull(message = "파일명은 필수입니다.")
+	@Size(min = 1, max = 50, message = "파일명은 1자 이상 50자 이하로 입력해야 합니다.")
 	String name,
 	String description
 ) {
